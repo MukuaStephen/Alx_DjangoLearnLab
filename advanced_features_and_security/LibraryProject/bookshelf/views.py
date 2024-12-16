887de89cdb275b940fe6a856fbc5ab435f094395
Stephen Mukua <mukuastephen08@gmail.com>
2024-12-16 17:04:32 +0300

    Check for views.py File Existence and Permission Decorators

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,15 +1,15 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Book
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseBadRequest
-from .forms import ExampleForm # Assume you have a form for Book
+from .forms import ExampleForm  # Assume you have a form for Book
 
-@login_required
+@login_required(raise_exception=True)
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'bookshelf/book_list.html', {'books': books})
 
-@login_required
+@login_required(raise_exception=True)
 def book_create(request):
     if request.method == 'POST':
         form = ExampleForm(request.POST)
@@ -20,7 +20,7 @@ def book_create(request):
         form = ExampleForm()
     return render(request, 'bookshelf/form_example.html', {'form': form})
 
-@login_required
+@login_required(raise_exception=True)
 def book_detail(request, pk):
     book = get_object_or_404(Book, pk=pk)
     return render(request, 'bookshelf/book_detail.html', {'book': book})
